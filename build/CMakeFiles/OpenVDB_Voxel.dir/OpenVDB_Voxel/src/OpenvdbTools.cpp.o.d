71dc1dad65a14a4e45717a3f38f9193c4c850b77
Paul Schuster <paul.schuster@tu-dresden.de>
2025-07-23 17:09:02 +0200

    exposes all ProcessWithDumptruck parameter to CLI removed tracking of build dir by git

--- build/CMakeFiles/OpenVDB_Voxel.dir/OpenVDB_Voxel/src/OpenvdbTools.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/OpenVDB_Voxel.dir/OpenVDB_Voxel/src/OpenvdbTools.cpp.o: \
- /mnt/c/Users/pschuster/source/repos/OpenVDB_Voxel/OpenVDB_Voxel/src/OpenvdbTools.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/OpenVDB_Voxel.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for OpenVDB_Voxel.
-# This may be replaced when dependencies are built.
